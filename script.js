0667aad49a834ff62e7c7cc518e9751f84aa8119
Payal Bisen <payal@gmail.com>
2026-09-22 18:08:20 +0530

    Refactor task management logic and enhance styling for better UI experience

--- script.js
+++ script.js
@@ -13,26 +13,16 @@ if(saveTask){
     tasks = JSON.parse(savedTasks)
 }
 
+// save task to lacal storage------
 
-addBtn.addEventListener("click", function(){
-    const task = taskInput.value.trim()
-
-    if (task === ""){
-        return
-    }
-
-    tasks.push(task)
-    taskInput.value = ""
-
-    saveTask()
-    renderTask()
-})
 function saveTask(){
     localStorage.setItem(
         "todos",
         JSON.stringify(tasks)
     )
 }
+
+// task display function----------------
 
 function renderTask(){
     taskList.innerHTML=""
@@ -48,4 +38,32 @@ function renderTask(){
         taskList.append(li)
     })
     taskCount.textContent=task.length
-}
+    }
+
+// 4. add task to the list when the add button is clicked
+
+    addBtn.addEventListener("click", function(){
+    const task = taskInput.value.trim()
+
+    if (task === ""){
+        return
+    }
+
+    tasks.push(task)
+    taskInput.value = ""
+
+    saveTask()
+    renderTask()
+})
+
+
+// Delete task form the list when the delete button is clicked
+
+taskList.addEventListener("click", function(event){
+    if(event.target.classList.contains("deleteBtn")){
+        const index = event.target.dataset.index
+        tasks.splice(index, 1)
+        saveTask()
+        renderTask()
+    }
+})
